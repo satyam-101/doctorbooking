95bd9b08f6ac8925ceaca032b31e8e4ffb30ce7d
satyam singh <satyamsingh20p@gmail.com>
2026-04-18 16:33:17 +0530

    enable cors

--- src/main/java/com/satyam/doctorbooking/config/SecurityConfig.java
+++ src/main/java/com/satyam/doctorbooking/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig {
             "http://localhost:8080",
             "http://localhost:5173",
             "http://127.0.0.1:5500",
-            "http://127.0.0.1:5173"
+            "http://127.0.0.1:5173",
+                "https://doctorbooking-frontend.vercel.app/"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
